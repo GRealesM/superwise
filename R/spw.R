4095ad402c4ad886e0d654bb68ed0041e7457944
GRealesM <solowars@gmail.com>
2017-10-15 18:19:25 -0200

    First upload

---
+++ R/spw.R
@@ -0,0 +1,170 @@
+#'The Superwise function
+#' 
+#' Pairwise statistical tests! Wiser than Supermatrix. This function allows you to perform multiple pairwise association tests between two sets of data, containing numeric/categorical and categorical data, respectively, and to adjust reulting p-values by different methods. See \code{details} for more information.
+#' @param x A data frame containing your data. See \code{details} for more information.
+#' @param v1 A range of columns containing phenotypic data in x. Data can be either quantitative (numeric) or qualitative (factor). Example: 1:20, for 20 columns of trait data, located in the first 20 columns.
+#' @param v2 A range of columns containing genotypic data in x. Example: 20:40, for 20 sites located in columns 20 to 40.
+#' @param fisher.method Indicates which method should be used for Fisher's tests. \code{"exact"} is default. For big datasets with a high number of categories, other methods might be useful. "sim" simulates p-values using 20000 Monte Carlo iterations, and "hybrid" will use hybrid method (see \code{\link{?fisher.test}} for more details on Fisher's methods).
+#' @param correction In case that p-value correction for multiple tests is required, use this parameter to specify which correction must be used. See ?p.adjust for details of available correction methods. It is possible to have more than one correction in one go, using a vector with correction names in the \code{p.adjust} function fashion (e.g. c("bonferroni", "fdr", "hochberg")). Each correction will add a column to the output.
+#' @param control Used as a internal control helper, to check that supermatrix performed the tests accordingly to variables' type.If TRUE, two columns indicating whether (1) the trait value was identified as numeric, and (2) the name of the test used for that column vs. genotypic data. 
+#'
+#' @return A data frame with the results of each test, including P-values and adjusted P-values (if requested).
+#'
+#' @details Superwise function was designed to perform multiple pairwise association analyses between two sets of phenotypic vs genotypic data. 
+#' 		Set1 must ideally contain phenotypic data (in columns), which can be quantitative (e.g. number of offspring, weight, etc) or categorical unordered (e.g. diurnal/nocturnal, herbivore/carnivore/omnivore) data. 
+#'		Set2 must ideally contain categorical unordered data, such as which amino-acid in a specific site in a proteic sequence  Such data should be distributed in columns, using rows for each species to be included in the analyses.  
+#' 		So far, superwise performs only Kruskal-Wallis test for numeric vs. categorical variables, and Fisher's Exact Test for categorical vs. categorical variables.
+#' 		The recommended format is a single data.frame containing the two sets of data to be compared pairwise. 
+#'		NOTE: Categorical data must be of factor class.
+
+spw <- function(x, v1, v2, fisher.method="exact", correction = NULL, control = TRUE){
+  #We start by dividing our dataframe and creating empty vectors
+  v1.tab <- x[,v1] #Dataframe with characteristics data
+  v2.tab <- x[,v2] #Dataframe with mutation data
+  Var1 <- character()
+  Var2 <- character()
+  numeric <- logical()
+  Test <- character()
+  FET.IC1 <- numeric()
+  FET.IC2 <- numeric()
+  FET.OR <- numeric()
+  KW.statistic <- numeric()
+  KW.df <- integer()
+  P.value <- numeric()
+  i <- 1 #Guide for vectors
+
+for(z in 1:length(v1.tab[1,])){
+    if (is.numeric(v1.tab[,z])==TRUE){
+      for (y in 1:length(v2.tab[1,])){
+        cc <- na.omit(cbind(v1.tab[,z], v2.tab[,y]))
+        if (nlevels(as.factor(cc[,2])) >= 2L){
+          KW <- kruskal.test(v1.tab[,z], v2.tab[,y])
+          Var1[i] <- names(v1.tab[z])
+          Var2[i] <- names(v2.tab[y])
+          numeric[i] <- TRUE
+          Test[i] <- "Kruskal-Wallis"
+          FET.IC1[i] <- NA
+          FET.IC2[i] <- NA
+          FET.OR[i] <- NA
+          KW.statistic[i] <- KW$statistic[[1]]
+          KW.df[i] <- KW$parameter[[1]]
+          P.value[i] <- KW$p.value
+          i <- i + 1
+        }
+        else {
+          Var1[i] <- names(v1.tab[z])
+          Var2[i] <- names(v2.tab[y])
+          is.numeric[i] <- TRUE
+          Test[i] <- "No test applied"
+          FET.IC1[i] <- NA
+          FET.IC2[i] <- NA
+          FET.OR[i] <- NA
+          KW.statistic[i] <- NA
+          KW.df[i] <- NA
+          P.value[i] <- NA
+          i <- i + 1
+              }
+      }
+    }
+    else{ for (y in 1:length(v2.tab[1,])){
+            cc <- na.omit(cbind(v1.tab[,z], v2.tab[,y]))
+            if (nlevels(as.factor(cc[,2])) >=2L){
+              if (fisher.method == "exact"){
+                fet <- fisher.test(table(v1.tab[,z], v2.tab[,y]), workspace = 2e5)
+                Var1[i] <- names(v1.tab[z])
+                Var2[i] <- names(v2.tab[y])
+                numeric[i] <- is.numeric(v1.tab[,z])
+                Test[i] <- "Fisher's Exact Test"
+                FET.IC1[i] <- fet$conf.int[[1]]
+                FET.IC2[i] <- fet$conf.int[[2]]
+                FET.OR[i] <- fet$estimate[[1]]
+                KW.statistic[i] <- NA
+                KW.df[i] <- NA
+                P.value[i] <- fet$p.value
+                i <- i + 1
+              }
+              if (fisher.method == "sim"){
+                fet <- fisher.test(table(v1.tab[,z], v2.tab[,y]), simulate.p.value = T, B = 20000)
+                Var1[i] <- names(v1.tab[z])
+                Var2[i] <- names(v2.tab[y])
+                numeric[i] <- is.numeric(v1.tab[,z])
+                Test[i] <- "Fisher's Exact Test (Sim)"
+                FET.IC1[i] <- fet$conf.int[[1]]
+                FET.IC2[i] <- fet$conf.int[[2]]
+                FET.OR[i] <- fet$estimate[[1]]
+                KW.statistic[i] <- NA
+                KW.df[i] <- NA
+                P.value[i] <- fet$p.value
+                i <- i + 1
+              }
+              if (fisher.method == "hybrid"){
+                fet <- fisher.test(table(v1.tab[,z], v2.tab[,y]), hybrid = T, workspace = 2e8)
+                Var1[i] <- names(v1.tab[z])
+                Var2[i] <- names(v2.tab[y])
+                numeric[i] <- is.numeric(v1.tab[,z])
+                Test[i] <- "Fisher's Exact Test (Hybrid)"
+                FET.IC1[i] <- fet$conf.int[[1]]
+                FET.IC2[i] <- fet$conf.int[[2]]
+                FET.OR[i] <- fet$estimate[[1]]
+                KW.statistic[i] <- NA
+                KW.df[i] <- NA
+                P.value[i] <- fet$p.value
+                i <- i + 1
+              }
+            }
+            else{
+              Var1[i] <- names(v1.tab[z])
+              Var2[i] <- names(v2.tab[y])
+              numeric[i] <- is.numeric(v1.tab[,z])
+              Test[i] <- "No test applied"
+              FET.IC1[i] <- NA
+              FET.IC2[i] <- NA
+              FET.OR[i] <- NA
+              KW.statistic[i] <- NA
+              KW.df[i] <- NA
+              P.value[i] <- NA
+              i <- i + 1
+            }
+          }
+        }
+}
+  df <- data.frame(Var1,Var2,numeric,Test, FET.IC1,FET.IC2, FET.OR, KW.statistic, KW.df, P.value)
+#Shall we adjust those P.values?
+  if (is.null(correction) == FALSE) {
+      ps <- as.vector(df$P.value)
+      for (e in 1:length(correction)){
+        if (correction[e] == "bonferroni"){
+          BF.P.value <- p.adjust(ps,method = correction[e])
+          df <- cbind(df,BF.P.value)
+        }
+        if (correction[e] == "hochberg"){
+          Hochberg.P.value <- p.adjust(ps,method = correction[e])
+          df <- cbind(df,Hochberg.P.value)
+        }
+        if (correction[e] == "holm"){
+          Holm.P.value <- p.adjust(ps,method = correction[e])
+          df <- cbind(df,Holm.P.value)
+        }
+        if (correction[e] == "hommel"){
+          Hommel.P.value <- p.adjust(ps,method = correction[e])
+          df <- cbind(df,Hommel.P.value)
+        }
+        if (correction[e] == "fdr"){
+          FDR.P.value <- p.adjust(ps,method = correction[e])
+          df <- cbind(df,FDR.P.value)
+        }
+        if (correction[e] == "BH"){
+          BH.P.value <- p.adjust(ps,method = correction[e])
+          df <- cbind(df,BH.P.value)
+        }
+        if (correction[e] == "BY"){
+          BY.P.value <- p.adjust(ps,method = correction[e])
+          df <- cbind(df,BY.P.value)
+        }
+      }
+  if (control == FALSE){
+    df <- df[,-c(3:4)]
+  }
+  return(df)
+  }
+}
---
+++ R/supermatrix.R
@@ -0,0 +1,96 @@
+#' The supermatrix function
+#' 
+#' Not really a matrix! This function allows you to perform multiple pairwise association tests between numerical/categorical variables (phenotypes) and aligned amino-acid sites (categorical), create a matrix of p-values and adjust them by several p-value adjustment methods. For numeric vs. categorical data, Kruskal-Wallis test is used. For categorical data, Fisher Exact test is used. See details for more information.
+#' 
+#' @param x A data frame containing your data. See details for more information.
+#' @param var A range of columns containing trait data (Set 1) in x. Data can be either quantitative (numeric) or qualitative (factor). Example: 1:20, for 20 columns of trait data, located in the first 20 columns.
+#' @param mut A range of columns containing genetypic data (Set 2) in x. Example: 20:40, for 20 sites located in columns 20 to 40.
+#' @param fisher.method Indicates which method should be used for fisher's tests. "exact" is default. For big datasets, other methods might be useful. "sim" simulates p-values using 20000 Monte Carlo iterations, and "hybrid" will use hybrid method (see ?fisher.test for more details on Fisher's methods).
+#' @param correction In case that p-value correction for multiple tests is required, use this parameter to specify which correction must be used. See \code{\link{?p.adjust}} for details of available correction methods.
+#' @param control Used as a internal control helper, to check that supermatrix performed the tests accordingly to variables' type.If TRUE, two columns indicating whether (1) the trait value was identified as numeric, and (2) the name of the test used for that column vs. genotypic data. 
+#' 
+#' @return A matrix-like dataframe with P-values for each test performed.
+#' 
+#' @details Supermatrix function was designed to perform multiple association analyses between phenotypic/life history traits, and proteic sites across multiple animal species. Such data should be distributed in columns, using rows for each species to be included in the analyses. The recommended format is a single data.frame containing the two sets of data to be compared pairwise. 
+#' 		Set1 must ideally contain phenotypic data (in columns), which can be quantitative (e.g. number of offspring, weight, etc) or categorical unordered (e.g. diurnal/nocturnal, herbivore/carnivore/omnivore) data. 
+#'		Set2 must ideally contain categorical unordered data, such as which amino-acid in a specific site in a proteic sequence.
+#' 		IMPORTANT: Categorical data must be of factor class.
+#' 		Since \code{supermatrix} only creates a data frame with P-values (which may be insufficient in most cases), we encourage using \code{\link{spw}} function instead.
+
+supermatrix <- function(x, var, mut, fisher.method = "exact", correction = NULL, control = TRUE){
+  #We create an empty data frame
+  rows <- length(var)
+  cols <- length(mut) + 2 #+2 in order to leave space for "Categorical" and "Test" control columns
+  df <- data.frame(matrix(nrow = rows, ncol = cols))
+  names.cols <- c("numeric", "model", names(x[,mut]))
+  names.rows <- names(x[,var])
+  names(df) <- names.cols
+  row.names(df) <- names.rows
+  #Let's interrogate about numeric!
+  for (i in 1:length(df[,1])) {
+    df[i,1] <- is.numeric(x[,i])
+  }  
+  #Now let's perform some statistical tests!
+  n <- 1 #row guide variable for df
+  m <- 3 #col guide variable for df
+  #v1 <- 1 #col guide variable for var.tab
+  m2 <- 1 #col guide variable for mut.tab
+  var.tab <- x[,var] #Dataframe with characteristics data
+  mut.tab <- x[,mut] #Dataframe with mutation data
+  for (l in 1:length(var.tab[1,])) { #To run the test row-wise
+    if (df[n,1] == TRUE) { #Numeric, not normal
+      df[n,2] <- "Kruskal-Wallis"
+      for (k in 1:length(mut.tab[1,])) { #Run column-wise
+        cc <- na.omit(cbind(var.tab[,n], mut.tab[,m2])) #To check that at least levels are kept after NA correction
+        if (nlevels(as.factor(cc[,2])) >= 2L) { #Run only if at least one mut is present after correction
+          KW <- kruskal.test(var.tab[,n], mut.tab[,m2])
+          df[n,m] <- KW$p.value
+        }
+        m <- m + 1
+        m2 <- m2 + 1
+      }
+    }
+    if (df[n,1] == FALSE) { #Categorical
+      df[n,2] <- "Fisher's exact test"
+      for (k in 1:length(mut.tab[1,])) {
+        cc <- na.omit(cbind(var.tab[,n], mut.tab[,m2]))
+        if (nlevels(as.factor(cc[,2])) >= 2L) {
+          if (fisher.method == "exact") { 
+            #print(paste("char", n))
+            #print(paste("mutation", m2))
+            fet <- fisher.test(table(var.tab[,n], mut.tab[,m2]), workspace = 2e5)
+            df[n,m] <- fet$p.value
+          }
+          
+          if (fisher.method == "sim") { 
+            fet <- fisher.test(table(var.tab[,n], mut.tab[,m2]),simulate.p.value = T, B = 20000)
+            df[n,m] <- fet$p.value
+          }
+          
+          if (fisher.method == "hybrid") { 
+            fet <- fisher.test(table(var.tab[,n], mut.tab[,m2]), hybrid = T, workspace = 2e8)
+            df[n,m] <- fet$p.value
+          }
+        }
+        m <- m + 1
+        m2 <- m2 + 1
+      }
+    }
+    m <- 3
+    m2 <- 1
+    n <- n + 1
+  }
+  #Finally we remove annoying constant NA columns in the results
+  df <- df[, !apply(is.na(df), 2, all)]
+  if (is.null(correction) == FALSE) {
+    ps <- as.vector(as.matrix(df[,3:ncol(df)]))
+    pcor <- p.adjust(ps, method = correction)
+    pcor <- matrix(pcor, nrow = nrow(df), ncol = ncol(df)-2)
+    df[,3:ncol(df)] <- pcor
+  }
+  if (control == FALSE){
+    df <- df[,-c(1:2)]
+  }
+  
+  return(df)
+}
